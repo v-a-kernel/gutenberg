2a19bfc5baf652d89daba18ddeb6845333bab9c7
iseulde <ella@iseulde.com>
2017-10-11 19:30:25 +0200

    Add vertical arrow navigation for blocks

--- editor/utils/dom.js
+++ editor/utils/dom.js
@@ -1,17 +1,17 @@
 /**
- * Check whether the selection touches an edge of the container
+ * Check whether the caret is horizontally at the edge of the container.
  *
- * @param  {Element} container DOM Element
- * @param  {Boolean} start     Reverse means check if it touches the start of the container
- * @return {Boolean}           Is Edge or not
+ * @param  {HTMLElement} container Focusable element.
+ * @param  {Boolean}     reverse   Set to true to check left, false for right.
+ * @return {Boolean}               True if at the edge, false if not.
  */
-export function isEdge( container, start = false ) {
+export function isHorizontalEdge( container, reverse ) {
 	if ( [ 'INPUT', 'TEXTAREA' ].indexOf( container.tagName ) !== -1 ) {
 		if ( container.selectionStart !== container.selectionEnd ) {
 			return false;
 		}
 
-		if ( start ) {
+		if ( reverse ) {
 			return container.selectionStart === 0;
 		}
 
@@ -24,21 +24,22 @@ export function isEdge( container, start = false ) {
 
 	const selection = window.getSelection();
 	const range = selection.rangeCount ? selection.getRangeAt( 0 ) : null;
-	const position = start ? 'start' : 'end';
-	const order = start ? 'first' : 'last';
-	const offset = range[ `${ position }Offset` ];
-
-	let node = range.startContainer;
 
 	if ( ! range || ! range.collapsed ) {
 		return false;
 	}
 
-	if ( start && offset !== 0 ) {
+	const position = reverse ? 'start' : 'end';
+	const order = reverse ? 'first' : 'last';
+	const offset = range[ `${ position }Offset` ];
+
+	let node = range.startContainer;
+
+	if ( reverse && offset !== 0 ) {
 		return false;
 	}
 
-	if ( ! start && offset !== node.textContent.length ) {
+	if ( ! reverse && offset !== node.textContent.length ) {
 		return false;
 	}
 
@@ -56,18 +57,81 @@ export function isEdge( container, start = false ) {
 }
 
 /**
- * Places the caret at start or end of a given element
+ * Holds the first caret boundary rectangle for subsequent calls.
  *
- * @param  {Element} container DOM Element
- * @param  {Boolean} start     Position: Start or end of the element
+ * @type {DOMRect}
  */
-export function placeCaretAtEdge( container, start = false ) {
+let firstVerticalRect;
+
+/**
+ * Resets the first caret position.
+ */
+export function resetVerticalPosition() {
+	firstVerticalRect = null;
+}
+
+/**
+ * Check whether the caret is vertically at the edge of the container.
+ *
+ * @param  {HTMLElement} container Focusable element.
+ * @param  {Boolean}     reverse   Set to true to check top, false for bottom.
+ * @return {Boolean}               True if at the edge, false if not.
+ */
+export function isVerticalEdge( container, reverse ) {
+	if ( [ 'INPUT', 'TEXTAREA' ].indexOf( container.tagName ) !== -1 ) {
+		return isHorizontalEdge( container, reverse );
+	}
+
+	if ( ! container.isContentEditable ) {
+		return true;
+	}
+
+	const selection = window.getSelection();
+	const range = selection.rangeCount ? selection.getRangeAt( 0 ) : null;
+
+	if ( ! range || ! range.collapsed ) {
+		return false;
+	}
+
+	const rangeRect = range.getClientRects()[ 0 ];
+
+	if ( ! rangeRect ) {
+		return false;
+	}
+
+	const buffer = rangeRect.height / 2;
+	const editableRect = container.getBoundingClientRect();
+
+	if ( ! firstVerticalRect ) {
+		firstVerticalRect = rangeRect;
+	}
+
+	// Too low.
+	if ( reverse && rangeRect.top - buffer > editableRect.top ) {
+		return false;
+	}
+
+	// Too high.
+	if ( ! reverse && rangeRect.bottom + buffer < editableRect.bottom ) {
+		return false;
+	}
+
+	return true;
+}
+
+/**
+ * Places the caret at start or end of a given element.
+ *
+ * @param {HTMLElement} container Focusable element.
+ * @param {Boolean}     reverse   True for end, false for start.
+ */
+export function placeCaretAtHorizontalEdge( container, reverse ) {
 	const isInputOrTextarea = [ 'INPUT', 'TEXTAREA' ].indexOf( container.tagName ) !== -1;
 
 	// Inputs and Textareas
 	if ( isInputOrTextarea ) {
 		container.focus();
-		if ( start ) {
+		if ( reverse ) {
 			container.selectionStart = 0;
 			container.selectionEnd = 0;
 		} else {
@@ -77,12 +141,61 @@ export function placeCaretAtEdge( container, start = false ) {
 		return;
 	}
 
+	if ( ! container.isContentEditable ) {
+		container.focus();
+		return;
+	}
+
 	// Content editables
 	const range = document.createRange();
 	range.selectNodeContents( container );
-	range.collapse( start );
+	range.collapse( reverse );
 	const sel = window.getSelection();
 	sel.removeAllRanges();
 	sel.addRange( range );
 	container.focus();
+}
+
+/**
+ * Places the caret at the top or bottom of a given element.
+ *
+ * @param {HTMLElement} container Focusable element.
+ * @param {Boolean}     reverse   True for bottom, false for top.
+ * @param {Boolean}     noScroll  Set to true to prevent scrolling.
+ */
+export function placeCaretAtVerticalEdge( container, reverse, noScroll ) {
+	const rect = firstVerticalRect;
+
+	if ( ! rect || ! container.isContentEditable ) {
+		placeCaretAtHorizontalEdge( container, reverse );
+		return;
+	}
+
+	const buffer = rect.height / 2;
+	const editableRect = container.getBoundingClientRect();
+	const x = rect.left;
+	const y = reverse ? ( editableRect.bottom - buffer ) : ( editableRect.top + buffer );
+	const range = document.caretRangeFromPoint( x, y );
+	const selection = window.getSelection();
+
+	if ( ! range ) {
+		if ( ! noScroll ) {
+			// Might be out of view.
+			// Easier than attempting to calculate manually.
+			container.scrollIntoView( reverse );
+			placeCaretAtVerticalEdge( container, reverse, true );
+		} else {
+			placeCaretAtHorizontalEdge( container, reverse );
+		}
+
+		return;
+	}
+
+	selection.removeAllRanges();
+	selection.addRange( range );
+	container.focus();
+	// To do: editable was already focussed, it goes back to old range...
+	// This fixes it.
+	selection.removeAllRanges();
+	selection.addRange( range );
 }
--- editor/writing-flow/index.js
+++ editor/writing-flow/index.js
@@ -7,7 +7,13 @@ import { keycodes, focus } from '@wordpress/utils';
 /**
  * Internal dependencies
  */
-import { isEdge, placeCaretAtEdge } from '../utils/dom';
+import {
+	isHorizontalEdge,
+	isVerticalEdge,
+	resetVerticalPosition,
+	placeCaretAtHorizontalEdge,
+	placeCaretAtVerticalEdge,
+} from '../utils/dom';
 
 /**
  * Module Constants
@@ -37,31 +43,63 @@ class WritingFlow extends Component {
 			) );
 	}
 
-	moveFocusInContainer( target, direction = 'UP' ) {
+	moveFocusInContainer( target, reverse, horizontal ) {
 		const focusableNodes = this.getVisibleTabbables();
-		if ( direction === 'UP' ) {
+
+		if ( reverse ) {
 			focusableNodes.reverse();
 		}
 
 		const targetNode = focusableNodes
 			.slice( focusableNodes.indexOf( target ) )
-			.reduce( ( result, node ) => {
-				return result || ( node.contains( target ) ? null : node );
+			.reduce( ( result, node, i, array ) => {
+				if ( result ) {
+					return result;
+				}
+
+				if ( node.contains( target ) ) {
+					return null;
+				}
+
+				const nextNode = array[ i + 1 ];
+
+				// Skip block node if it contains a focusable node.
+				if ( node.classList.contains( 'editor-visual-editor__block' ) && nextNode && node.contains( nextNode ) ) {
+					return null;
+				}
+
+				return node;
 			}, null );
 
 		if ( targetNode ) {
-			placeCaretAtEdge( targetNode, direction === 'DOWN' );
+			if ( horizontal ) {
+				placeCaretAtHorizontalEdge( targetNode, reverse );
+			} else {
+				placeCaretAtVerticalEdge( targetNode, reverse );
+			}
 		}
 	}
 
 	onKeyDown( event ) {
 		const { keyCode, target } = event;
-		const moveUp = ( keyCode === UP || keyCode === LEFT );
-		const moveDown = ( keyCode === DOWN || keyCode === RIGHT );
+		const up = keyCode === UP;
+		const down = keyCode === DOWN;
+		const left = keyCode === LEFT;
+		const right = keyCode === RIGHT;
+		const reverse = up || left;
+		const horizontal = left || right;
+		const vertical = up || down;
 
-		if ( ( moveUp || moveDown ) && isEdge( target, moveUp ) ) {
+		if (
+			( horizontal && isHorizontalEdge( target, reverse ) ) ||
+			( vertical && isVerticalEdge( target, reverse ) )
+		) {
 			event.preventDefault();
-			this.moveFocusInContainer( target, moveUp ? 'UP' : 'DOWN' );
+			this.moveFocusInContainer( target, reverse, horizontal );
+		}
+
+		if ( ! vertical ) {
+			resetVerticalPosition();
 		}
 	}
 
@@ -71,7 +109,9 @@ class WritingFlow extends Component {
 		return (
 			<div
 				ref={ this.bindContainer }
-				onKeyDown={ this.onKeyDown }>
+				onKeyDown={ this.onKeyDown }
+				onMouseDown={ resetVerticalPosition }
+			>
 				{ children }
 			</div>
 		);
